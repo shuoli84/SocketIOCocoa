f3684dc64fa10f83b4b3e227bc4067245efd210e
shuo li <shuoli84@gmail.com>
2014-11-01 11:28:56 +0800

    Initial Commit

---
+++ SocketIOCocoa/SocketIOCocoa.h
@@ -0,0 +1,19 @@
+//
+//  SocketIOCocoa.h
+//  SocketIOCocoa
+//
+//  Created by LiShuo on 14/11/1.
+//  Copyright (c) 2014年 LiShuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SocketIOCocoa.
+FOUNDATION_EXPORT double SocketIOCocoaVersionNumber;
+
+//! Project version string for SocketIOCocoa.
+FOUNDATION_EXPORT const unsigned char SocketIOCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketIOCocoa/PublicHeader.h>
+
+
---
+++ SocketIOCocoaTests/SocketIOCocoaTests.swift
@@ -0,0 +1,36 @@
+//
+//  SocketIOCocoaTests.swift
+//  SocketIOCocoaTests
+//
+//  Created by LiShuo on 14/11/1.
+//  Copyright (c) 2014年 LiShuo. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SocketIOCocoaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
